35729810acdf72018d140c00c9e5f694b2c63f81
Decayy25 <mochrizqihermawan615@gmail.com>
2026-01-21 09:37:14 +0700

    add navbar observer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,24 @@
+// navbar
 const toggle_menu = () => {
     document.getElementById('mobile-menu').classList.toggle('hidden')
 }
 
 // navbar observer
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-link");
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      navLinks.forEach(link => link.classList.remove("text-blue-500", "font-bold", "underline", "decoration-3", "underline-offset-4"));
+
+      const activeLink = document.querySelector(`.nav-link[href="#${entry.target.id}"]`);
+      if (activeLink) {
+        activeLink.classList.add("text-blue-500", "font-bold", "underline", "decoration-2", "underline-offset-4");
+      }
+    }
+  });
+}, { threshold: 0.6 });
+
+
+sections.forEach(section => observer.observe(section));
