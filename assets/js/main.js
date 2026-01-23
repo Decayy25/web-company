6b9cd9cac1754deb189cbf6006bb1d962e5398f8
Decayy25 <mochrizqihermawan615@gmail.com>
2026-01-23 21:32:56 +0700

    add about

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,43 @@
 // navbar
-const toggle_menu = () => {
-    document.getElementById('mobile-menu').classList.toggle('hidden')
+function toggle_menu() {
+    document.getElementById('mobile-menu').classList.toggle('hidden');
+}
+
+// about
+const aboutLeft = document.getElementById('about-left');
+
+const members = [
+  {
+    title: "Kurikulum",
+    desc: "Kurikulum yang digunakan saat ini",
+    img: "assets/img/profile.png",
+    href: "pages/kurikulum.html"
+  },
+  {
+    title: "Jurusan",
+    desc: "Jurusan yang tersedia saat ini.",
+    img: "assets/img/profile.png",
+    href: "pages/jurusan.html"
+  },
+  {
+    title: "Jadwal Pelajaran",
+    desc: "",
+    img: "assets/img/profile.png",
+    href: ""
+  },
+];
+
+if (aboutLeft) {
+  aboutLeft.innerHTML = members.map(member => `
+    <a href="${member.href}" class="block">
+      <div class="about-box p-4 text-center hover:scale-105 transition">
+        <div class="w-32 h-32 rounded-full overflow-hidden border-4 border-[#020617] mx-auto mb-4">
+          <img src="${member.img}" class="w-full h-full object-cover">
+        </div>
+
+        <h3 class="text-lg font-semibold">${member.title}</h3>
+        <p class="text-sm text-slate-300 mt-2">${member.desc}</p>
+      </div>
+    </a>
+  `).join('');
 }
