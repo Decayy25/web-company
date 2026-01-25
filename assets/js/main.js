3cbfa3ea78d95f38eedf794210fe855db3c8b69a
Decayy25 <mochrizqihermawan615@gmail.com>
2026-01-25 17:44:57 +0700

    add footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,36 +12,41 @@ const members = [
     desc: "visi-misi yang digunakan saat ini.",
     img: "assets/img/profile.png",
     href: "pages/visi-misi.html",
-    aos: "fade-down-right"
+    aos: "fade-down-right",
+    rotate: "-9deg"
   },
   {
     title: "Informasi Jurusan",
     desc: "Jurusan yang tersedia saat ini.",
     img: "assets/img/profile.png",
     href: "pages/jurusan.pdf",
-    aos: "fade-down-left"
+    aos: "fade-down-left",
+    rotate: "9deg"
   },
   {
     title: "Jadwal Pelajaran",
     desc: "Jadwal yang digunakan saat ini.",
     img: "assets/img/profile.png",
     href: "pages/jadwal.pdf",
-    aos: "fade-up-right"
+    aos: "fade-up-right",
+    rotate: "-9deg"
   },
   {
     title: "Brosur PPDB 2024",
     desc: "Download Brosur",
     img: "assets/img/profile.png",
     href: "pages/brosur.pdf",
-    aos: "fade-up-left"
+    aos: "fade-up-left",
+    rotate: "9deg"
   }
 ];
 
 if (aboutLeft) {
   aboutLeft.innerHTML = members.map(member => `
     <a href="${member.href}" class="block" target="_blank" data-aos="${member.aos}">
-      <div class="about-box p-4 text-center hover:scale-105 transition">
-        <div class="w-32 h-32 rounded-full overflow-hidden border-4 border-[#020617] hover:border-[#51a2ff] mx-auto mb-4">
+      <div class="about-box p-4 text-center hover:scale-105 transition-transform 
+      duration-300 hover:rotate-[${member.rotate}]">
+        <div class="w-32 h-32 rounded-full overflow-hidden border-4 border-[#51a2ff] mx-auto mb-4">
           <img src="${member.img}" class="w-full h-full object-cover">
         </div>
 
